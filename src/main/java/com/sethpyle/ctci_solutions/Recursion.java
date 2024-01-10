0602a818dbd75035322f614154c3c6f9b58d939f
Seth Pyle <seth.pyle@churchofjesuschrist.org>
2024-01-09 20:43:55 -0600

    added robot grid problem

--- src/main/java/com/sethpyle/ctci_solutions/Recursion.java
+++ src/main/java/com/sethpyle/ctci_solutions/Recursion.java
@@ -1,11 +1,56 @@
 package com.sethpyle.ctci_solutions;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Recursion {
-  // A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time
-  // Implement a method to count how many possible ways the child can run up the stairs.
+  /**
+   * A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time
+   * Implement a method to count how many possible ways the child can run up the stairs.
+  */
   public static int staircaseProblem(int stepsLeft) {
     return (stepsLeft / 3 > 0 ? staircaseProblem(stepsLeft - 3) : 0)
         + (stepsLeft / 2 > 0 ? staircaseProblem(stepsLeft - 2) : 0)
         + (stepsLeft > 0 ? staircaseProblem(stepsLeft - 1) : 1);
+  }
+
+  record RobotCoordinate(int x, int y) {}
+
+  /**
+   * Imagine a robot sitting in the upper left corner of grid with r rows and c columns. The robot can only move
+   * in 2 directions, right and down, but certain cells are "off limits" such that the robot cannot step on them.
+   * Design an algorithm to find a path for the robot from the top left to the bottom right.
+   */
+  public static boolean robotInAGrid(
+      List<List<Boolean>> grid,
+      RobotCoordinate currentCoordinate,
+      Stack<RobotCoordinate> path) {
+    if (currentCoordinate.y() == grid.size() - 1) {
+      if (currentCoordinate.x() == grid.get(0).size() - 1) {
+        path.push(currentCoordinate);
+        return true;
+      }
+    }
+
+    if (currentCoordinate.y() < grid.size() - 1) {
+      var newCoordinate = new RobotCoordinate(currentCoordinate.x(), currentCoordinate.y() + 1);
+      if (grid.get(newCoordinate.y()).get(currentCoordinate.x())) {
+        if (robotInAGrid(grid, newCoordinate, path)) {
+          path.push(currentCoordinate);
+          return true;
+        }
+      }
+    }
+
+    if (currentCoordinate.x() < grid.get(currentCoordinate.x()).size() - 1) {
+      var newCoordinate = new RobotCoordinate(currentCoordinate.x() + 1, currentCoordinate.y());
+      if (grid.get(newCoordinate.y()).get(newCoordinate.x())) {
+        if (robotInAGrid(grid, newCoordinate, path)) {
+          path.push(currentCoordinate);
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
--- src/test/java/com/sethpyle/ctci_solutions/RecursionTest.java
+++ src/test/java/com/sethpyle/ctci_solutions/RecursionTest.java
@@ -2,6 +2,9 @@ package com.sethpyle.ctci_solutions;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Stack;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RecursionTest {
@@ -12,5 +15,87 @@ public class RecursionTest {
     assertThat(Recursion.staircaseProblem(3)).isEqualTo(4);
     assertThat(Recursion.staircaseProblem(4)).isEqualTo(7);
     assertThat(Recursion.staircaseProblem(5)).isEqualTo(13);
+  }
+
+  @Test
+  public void testRobotInAGrid() {
+    // T F F
+    // T F F
+    // T T T
+    var testGrid1 = List.of(
+        List.of(true, false, false),
+        List.of(true, false, false),
+        List.of(true, true, true)
+    );
+
+    var startingCoordinate = new Recursion.RobotCoordinate(0, 0);
+    var path = new Stack<Recursion.RobotCoordinate>();
+
+    Recursion.robotInAGrid(testGrid1, startingCoordinate, path);
+
+    assertThat(path).hasSize(5);
+    assertThat(path).containsExactly(
+        new Recursion.RobotCoordinate(2, 2),
+        new Recursion.RobotCoordinate(1, 2),
+        new Recursion.RobotCoordinate(0, 2),
+        new Recursion.RobotCoordinate(0, 1),
+        new Recursion.RobotCoordinate(0, 0)
+    );
+
+    // T F F F F
+    // T T F F F
+    // F T T T F
+    // F F F T T
+    // F F F F T
+
+    var testGrid2 = List.of(
+        List.of(true, false, false, false, false),
+        List.of(true, true, false, false, false),
+        List.of(false, true, true, true, false),
+        List.of(false, false, false, true, true),
+        List.of(false, false, false, false, true)
+    );
+
+    path = new Stack<>();
+
+    Recursion.robotInAGrid(testGrid2, startingCoordinate, path);
+
+    assertThat(path).hasSize(9);
+    assertThat(path).containsExactly(
+        new Recursion.RobotCoordinate(4, 4),
+        new Recursion.RobotCoordinate(4, 3),
+        new Recursion.RobotCoordinate(3, 3),
+        new Recursion.RobotCoordinate(3, 2),
+        new Recursion.RobotCoordinate(2, 2),
+        new Recursion.RobotCoordinate(1, 2),
+        new Recursion.RobotCoordinate(1, 1),
+        new Recursion.RobotCoordinate(0, 1),
+        new Recursion.RobotCoordinate(0, 0)
+    );
+
+    // T T T T
+    // F F F T
+    // F F F T
+
+    var testGrid3 = List.of(
+        List.of(true, true, true, true),
+        List.of(false, false, false, true),
+        List.of(false, false, false, true)
+    );
+
+    path = new Stack<>();
+
+    Recursion.robotInAGrid(testGrid3, startingCoordinate, path);
+
+    assertThat(path).hasSize(6);
+
+    assertThat(path).containsExactly(
+        new Recursion.RobotCoordinate(3, 2),
+        new Recursion.RobotCoordinate(3, 1),
+        new Recursion.RobotCoordinate(3, 0),
+        new Recursion.RobotCoordinate(2, 0),
+        new Recursion.RobotCoordinate(1, 0),
+        new Recursion.RobotCoordinate(0, 0)
+    );
   }
 }
